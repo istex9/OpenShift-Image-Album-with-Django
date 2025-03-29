67ee5b6d3a48088804053d25e34817a9e7c34413
iDomOSx <47350544+istex9@users.noreply.github.com>
2025-03-29 10:42:08 +0100

    Fix project structure

---
+++ settings.py
@@ -0,0 +1,28 @@
+import os
+
+INSTALLED_APPS = [
+    # ...
+    'django.contrib.staticfiles',
+    'gallery',  # Saját app hozzáadva
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT', 5432),
+    }
+}
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
