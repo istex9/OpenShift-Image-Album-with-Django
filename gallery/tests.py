2057b02d712842688e594940f2820dce459c8053
iDomOSx <47350544+istex9@users.noreply.github.com>
2025-03-29 14:00:45 +0100

    add test.py

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class GalleryTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='tesztuser', password='tesztjelszo')
+
+    def test_login_required_for_upload(self):
+        response = self.client.get(reverse('upload_photo'))
+        self.assertRedirects(response, f'/login/?next={reverse("upload_photo")}')
+
+    def test_homepage_accessible(self):
+        self.client.login(username='tesztuser', password='tesztjelszo')
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Fényképek listája")
