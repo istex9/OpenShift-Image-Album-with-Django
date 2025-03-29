3965478daca27ae6b3f91044382cb6fcbff43e6b
iDomOSx <47350544+istex9@users.noreply.github.com>
2025-03-29 13:22:15 +0100

    add bootstrap

--- photoalbum/urls.py
+++ photoalbum/urls.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('gallery.urls')),
+    
+    path('', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+
+    path('gallery/', include('gallery.urls')),
+
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
